2d145a0fc1f2d705fc000ffa2aa754702d51fea1
KayterCantFixBug <kienductrong@gmail.com>
2023-02-22 10:37:10 +0700

    first commit

---
+++ Test/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using static Test.Form1;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
